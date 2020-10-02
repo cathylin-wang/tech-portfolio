2aa2aade3c7d647eca2ab80984fcf889ed7178bb
cathylin-wang <57296869+cathylin-wang@users.noreply.github.com>
2020-10-02 14:02:18 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Cathylin Wang', // Title for your website.
+    tagline: 'Aspiring Software Engineer. Data Enthusiast.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://github.com/cathylin-wang', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
